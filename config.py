89cc0ca237fd8d4654dc990e8d65f2eedc3ee3ef
Steve <blackmustangsally1970@gmail.com>
2026-02-09 11:43:25 +1000

    Add ClickSend sender config keys + prep for invite test

--- config.py
+++ config.py
@@ -4,6 +4,15 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "devkey")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # ClickSend SMS configuration
+    CLICKSEND_USERNAME = os.environ.get("CLICKSEND_USERNAME", "")
+    CLICKSEND_API_KEY = os.environ.get("CLICKSEND_API_KEY", "")
+
+    # Dedicated sender numbers
+    EQUESTRIAN_SENDER = os.environ.get("EQUESTRIAN_SENDER", "")
+    WEDDING_SENDER = os.environ.get("WEDDING_SENDER", "")
+
+
     # Build DB URI at class level so Flask can read it
     db_url = os.environ.get("SQLALCHEMY_DATABASE_URI") or \
              os.environ.get("DATABASE_URL") or \
