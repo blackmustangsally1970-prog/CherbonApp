56d91a65eeb5e3dc8816a8d7e03d4bdaaed144af
Steve <blackmustangsally1970@gmail.com>
2026-03-03 17:03:54 +1000

    disclaimer

--- models.py
+++ models.py
@@ -136,6 +136,17 @@ class TeacherHorse(db.Model):
     horse1 = db.Column(db.String(100))
     horse2 = db.Column(db.String(100))
 
+class Users(db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    role = db.Column(db.String(20), default="management")
+
+
+
 
 class LessonBlockTag(db.Model):
     __tablename__ = 'lesson_block_tags'
