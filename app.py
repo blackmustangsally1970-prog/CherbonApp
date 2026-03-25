7b7d88049b8bbe1e97140e36d97e5f4ef6189f58
Steve <blackmustangsally1970@gmail.com>
2026-03-25 19:11:11 +1000

    menu2

--- app.py
+++ app.py
@@ -4772,6 +4772,23 @@ Cherbon Waters Admin
         return render_template("messages_menu.html")
 
 
+    @app.route("/other_tools")
+    def other_tools():
+        return render_template("other_tools.html")
+
+    @app.route("/manage_blockout_dates")
+    def manage_blockout_dates():
+        return render_template("manage_blockout_dates.html")
+
+    @app.route("/manage_blockout_ranges")
+    def manage_blockout_ranges():
+        return render_template("manage_blockout_ranges.html")
+
+    @app.route("/manage_teacher_times")
+    def manage_teacher_times():
+        return render_template("manage_teacher_times.html")
+
+
     @app.route('/client_view')
     def client_view():
         client_id = request.args.get('client', type=int) or request.args.get('client_filter', type=int)
