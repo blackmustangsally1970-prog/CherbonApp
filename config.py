8adf83d010f5c7e190b700775ebbc30590a559d3
Steve <blackmustangsally1970@gmail.com>
2026-03-27 10:16:47 +1000

    MB6

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     EQUESTRIAN_SENDER = os.environ.get("EQUESTRIAN_SENDER", "")
     WEDDING_SENDER = os.environ.get("WEDDING_SENDER", "")
 
+    SMS_ADMIN_NUMBER = "+61430151331"  
 
     # Build DB URI at class level so Flask can read it
     db_url = os.environ.get("SQLALCHEMY_DATABASE_URI") or \
