39fb26e5c17c2d2829eac64d090e34768e26034b
Steve <blackmustangsally1970@gmail.com>
2026-04-29 09:52:17 +1000

    Save2

--- app.py
+++ app.py
@@ -4722,7 +4722,6 @@ def create_app():
 
         try:
             # Always start clean
-            db.session.rollback()
 
             # --------------------------------------------------------
             # SAVE LESSONS
