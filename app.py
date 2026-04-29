44a434c721dfad3e05876b53e5952a1301a2f855
Steve <blackmustangsally1970@gmail.com>
2026-04-29 14:07:26 +1000

    superF7

--- app.py
+++ app.py
@@ -1351,6 +1351,7 @@ def create_app():
 
     @app.route("/superlookup", methods=["GET", "POST"])
     def superlookup():
+        print(">>> SUPERLOOKUP ROUTE HIT")
         q = request.form.get("q", "").strip()
         results = []
 
