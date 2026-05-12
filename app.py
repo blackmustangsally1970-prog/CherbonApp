5104e7fc758a07e119fe543c321cce2159078d71
Steve <blackmustangsally1970@gmail.com>
2026-05-12 15:36:26 +1000

    Gift Voucher8

--- app.py
+++ app.py
@@ -3484,7 +3484,10 @@ def create_app():
             flash("Missing JotForm API key", "danger")
             return redirect(url_for("gift_vouchers"))
 
+
+
         url = f"https://us-api.jotform.com/form/{GIFT_VOUCHER_FORM_ID}/submissions?apiKey={api_key}"
+        print("DEBUG FETCH URL:", url)
         response = requests.get(url)
         data = response.json()
 
