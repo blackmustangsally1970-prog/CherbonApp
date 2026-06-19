343cdf9ec082a06ee04277ffd32797ed4c797975
Steve <blackmustangsally1970@gmail.com>
2026-06-19 21:07:15 +1000

    Receipt9

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import (
     jsonify, current_app, Response, session
 )
 
-from flask_login import current_user
+from flask_login import LoginManager, current_user
 
 from collections import defaultdict
 from collections import Counter
@@ -1531,6 +1531,15 @@ def parse_jotform_payload(payload, forced_submission_id=None, clients_cache=None
 def create_app():
     print(">>> JOTFORM KEY IN APP:", os.getenv("JOTFORM_API_KEY"))
     app = Flask(__name__)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "")
